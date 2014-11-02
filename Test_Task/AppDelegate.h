5b76e64300c794ce050c9e138a5ea69cb5c26b4a
gavrilak <gavrilak89@gmail.com>
2014-11-02 19:25:22 +0200

    Initial Commit

---
+++ Test_Task/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test_Task
+//
+//  Created by Dima on 11/2/14.
+//  Copyright (c) 2014 Dima Soldatenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
